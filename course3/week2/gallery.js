91605b3703efbb9e911e5c6b449d100f9c48c936
Nandini2209 <69377893+Nandini2209@users.noreply.github.com>
2020-08-11 11:46:50 +0530

    Add files via upload

---
+++ course3/week2/gallery.js
@@ -0,0 +1,10 @@
+function upDate(previewPic){
+	x=document.getElementById("image");
+  x.innerHTML=previewPic.alt;
+  x.style.backgroundImage="url("+previewPic.src+")";
+}
+function unDo(X){
+	x=document.getElementById("image");
+  x.innerHTML=previewPic.alt;
+  x.style.backgroundImage="url("+previewPic.src+")";
+}	  
